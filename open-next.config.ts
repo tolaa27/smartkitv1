9b39c6d703154ce0c43e5ea4ee30efcd682455d3
tolaa27 <harikitopcam@gmail.com>
2026-09-24 13:35:51 +0700

    fix(deploy): remove R2 cache binding to allow deployment without R2 subscription

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-const config = defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
-});
+const config = defineCloudflareConfig();
 
 export default {
 	...config,
